73d95dd61be3352e2ae47a70fd873a5a6eaecd80
PATRICKFLANAGAN-STUD <g00431255@atu.ie>
2025-01-27 11:21:08 +0000

    started code

---
+++ src/main/java/ie/atu/week2sem2/JustApp.java
@@ -0,0 +1,14 @@
+package ie.atu.week2sem2;
+
+public class JustApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to JustApp");
+        System.out.println("This is your order...");
+        //Create an order
+
+
+        //List all of food items ie burgers,pizzas,chips
+
+        //Display details to the user
+    }
+}
